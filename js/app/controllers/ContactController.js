70a40b2273cd68e3202e23f028cde86297a40a4a
Joe Kim <josephkim0224@gmail.com>
2017-09-14 07:35:48 -0400

    Done

--- js/app/controllers/ContactController.js
+++ js/app/controllers/ContactController.js
@@ -13,6 +13,17 @@ function ContactController() {
     this.removeContact = function (index) {
         this.contacts.splice(index, 1);
     }
+
+    this.addContacts = function() {
+        this.contacts.splice(0, 1);
+    }
+
+    // this.addContact = function () {
+    //     this.contacts.push([{
+    //         name: "EE",
+    //         phone: "!!!!"
+    //     }]);
+    // }
 }
 
 angular
